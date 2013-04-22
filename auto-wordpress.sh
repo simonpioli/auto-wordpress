1a4c8e84fc5734959cbc8544c570527415682829
Simon Pioli <simon.pioli@citypress.co.uk>
2013-04-22 15:50:24 +0100

    Getting themes, commented out plugins I don't often use

--- auto-wordpress.sh
+++ auto-wordpress.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#
-# 	Wordpress Installer
-# 	------------------
-# 	Downloads and installs latest version of WordPress with choice of base themes and selection of common plug-ins.
-#		
-# 	To use this script, go to the directory you want to install Wordpress to in the terminal and run this command:
-# 
-# 	curl -O https://raw.github.com/simonpioli/auto-wordpress/master/auto-wordpress.sh | bash
-#
-# 	There you go.
-#
-
 
 # Latest version of WP
 echo "Fetching WordPress...";
@@ -18,23 +6,27 @@ curl -O http://wordpress.org/latest.zip;
 unzip -q latest.zip;
 
 # Base Theme
-echo "Which base theme? bones, underscore"
+echo "Which base theme? 'bones', 'underscore' or 'none' if you don't want a base theme."
 read theme
-echo "What should we call it?"
-read newname
 
-if [ $theme == "bones" ]; then
-	echo "Fetching Bones...";
-	curl -O https://github.com/eddiemachado/bones/archive/master.zip;
-	unzip -q bones-master.zip;
-	mv bones-master wordpress/wp-content/themes/
-	mv wordpress/wp-content/themes/bones-master wordpress/wp-content/themes/$newname
-elif [ $theme == "underscore" ]; then
-	echo "Fetching Underscore...";
-	curl -O https://github.com/Automattic/_s/archive/master.zip;
-	unzip -q _s-master.zip;
-	mv _s-master wordpress/wp-content/themes/
-	mv wordpress/wp-content/themes/_s-master wordpress/wp-content/themes/$newname
+if [ $theme != "none" ]; then
+	echo "What should we call it?"
+	read newname
+
+	if [ $theme == "bones" ]; then
+		echo "Fetching Bones...";
+		# curl -L -o $newname.zip https://github.com/repos/eddiemachado/bones/zipball/master
+		curl -L -o $newname.tar.gz https://api.github.com/repos/eddiemachado/bones/tarball;
+		tar -zxvf $newname.tar.gz;
+		mv *bones* wordpress/wp-content/themes/
+		mv wordpress/wp-content/themes/*bones* wordpress/wp-content/themes/$newname
+	elif [ $theme == "underscore" ]; then
+		echo "Fetching Underscore...";
+		curl -L -o newname.tar.gz https://api.github.com/repos/Automattic/_s/tarball;
+		tar -zxvf $newname.tar.gz;
+		mv *_s* wordpress/wp-content/themes/
+		mv wordpress/wp-content/themes/*_s* wordpress/wp-content/themes/$newname
+	fi
 fi
 
 # All-in-One-SEO-Pack
@@ -56,16 +48,16 @@ unzip -q  secure-wordpress.zip;
 mv secure-wordpress wordpress/wp-content/plugins/
 
 # Hierarchy Plugin
-echo "Fetching Hierarchy plugin...";
-curl -O http://downloads.wordpress.org/plugin/hierarchy.zip;
-unzip -q  hierarchy.zip;
-mv hierarchy wordpress/wp-content/plugins/
+# echo "Fetching Hierarchy plugin...";
+# curl -O http://downloads.wordpress.org/plugin/hierarchy.zip;
+# unzip -q  hierarchy.zip;
+# mv hierarchy wordpress/wp-content/plugins/
 
 # Image Widgets (Why isn't this standard?)
-echo "Fetching Image Widget plugin...";
-curl -O http://downloads.wordpress.org/plugin/image-widget.zip;
-unzip -q  image-widget.zip;
-mv image-widget wordpress/wp-content/plugins/
+# echo "Fetching Image Widget plugin...";
+# curl -O http://downloads.wordpress.org/plugin/image-widget.zip;
+# unzip -q  image-widget.zip;
+# mv image-widget wordpress/wp-content/plugins/
 
 # Super-cache
 echo "Fetching Super Cache plugin...";
@@ -116,10 +108,10 @@ unzip -q wordpress-importer.zip
 mv wordpress-importer wordpress/wp-content/plugins/
 
 # WP-Quick-Pages
-echo "Fetching WP-Quick-Pages...";
-curl -O http://downloads.wordpress.org/plugin/wp-quick-pages.zip
-unzip -q wp-quick-pages.zip
-mv wp-quick-pages wordpress/wp-content/plugins/
+# echo "Fetching WP-Quick-Pages...";
+# curl -O http://downloads.wordpress.org/plugin/wp-quick-pages.zip
+# unzip -q wp-quick-pages.zip
+# mv wp-quick-pages wordpress/wp-content/plugins/
 
 # Simple Page Ordering
 echo "Fetching Simple Page Ordering...";
@@ -136,6 +128,7 @@ mv feedwordpress wordpress/wp-content/plugins/
 # Cleanup
 echo "Cleaning up temporary files and directories...";
 rm *.zip
+rm *.tar.gz
 
 # Move stuff into current directory
 mv wordpress/* .;
